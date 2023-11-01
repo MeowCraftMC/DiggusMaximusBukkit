6c986288a39817b91337dbfcead6b7ab8b8bcab5
秋雨落 <i@rain.cx>
2023-11-01 20:50:07 +0800

    :sparkles: Ver 1.20.2 supported!
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/cx/rain/mc/diggus_maximus_bukkit/nms/ExcavatorFactory.java
+++ src/main/java/cx/rain/mc/diggus_maximus_bukkit/nms/ExcavatorFactory.java
@@ -2,6 +2,7 @@ package cx.rain.mc.diggus_maximus_bukkit.nms;
 
 import cx.rain.mc.diggus_maximus_bukkit.nms.common.IExcavator;
 import cx.rain.mc.diggus_maximus_bukkit.nms.v1_19_4.ExcavatorImplV1_19_4;
+import cx.rain.mc.diggus_maximus_bukkit.nms.v1_20_2.ExcavatorImplV1_20_2;
 import org.bukkit.Bukkit;
 
 public class ExcavatorFactory {
@@ -10,13 +11,10 @@ public class ExcavatorFactory {
     static {
         var bukkitVersion = Bukkit.getBukkitVersion();
         switch (bukkitVersion) {
-            case "1.20-R0.1-SNAPSHOT":
-            case "1.20.1-R0.1-SNAPSHOT":
-            case "1.19.4-R0.1-SNAPSHOT":
-                EXCAVATOR = new ExcavatorImplV1_19_4();
-                break;
-            default:
-                throw new RuntimeException("Not supported version:" + bukkitVersion);
+            case "1.19.4-R0.1-SNAPSHOT", "1.20-R0.1-SNAPSHOT", "1.20.1-R0.1-SNAPSHOT" ->
+                    EXCAVATOR = new ExcavatorImplV1_19_4();
+            case "1.20.2-R0.1-SNAPSHOT" -> EXCAVATOR = new ExcavatorImplV1_20_2();
+            default -> throw new RuntimeException("Not supported version:" + bukkitVersion);
         }
     }
 
